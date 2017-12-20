df699d47db2911c9094a42f78bf273fa3117e4ff
Mark <496347267@qq.com>
2017-12-20 21:25:45 +0800

    创建Medoo核心类

---
+++ Medoo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 2017/12/20
+ * Time: 下午9:20
+ */
+namespace Medoo;
+use PDO;
+use Exception;
+use PDOException;
+
+class Medoo{
+    protected $database_type; //数据库类型
+}
